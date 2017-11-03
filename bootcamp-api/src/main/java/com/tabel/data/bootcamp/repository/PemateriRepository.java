b63c8c3327d94e2fa2fd8d28db7f1a04b9b928bd
secondson <mail.secondson@gmail.com>
2017-11-03 10:51:07 +0700

    Jadi Pakai Thymeleaf, masih ada Error di Authentication, tapi tidak ada pemberitahuan Error

--- bootcamp-api/src/main/java/com/tabel/data/bootcamp/repository/PemateriRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tabel.data.bootcamp.repository;
-
-import com.tabel.data.bootcamp.model.Pemateri;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PemateriRepository extends PagingAndSortingRepository<Pemateri, String>{
-
-}
--- bootcamp-api/src/main/java/com/tabel/data/bootcamp/repository/SekretarisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tabel.data.bootcamp.repository;
-
-import com.tabel.data.bootcamp.model.Sekretaris;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface SekretarisRepository extends PagingAndSortingRepository<Sekretaris, String>{
-
-}
